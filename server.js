5103ee2d47a0bb7d93b0061cdf798f8ba712974c
jordanliu <jordanxavierliu@gmail.com>
2020-02-01 00:34:59 -0500

    Added MongoDB connection

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+app.listen(port, () => {
+    console.log(
+        `Server is running on port: ${port} | https://localhost:${port}`
+    );
+});
