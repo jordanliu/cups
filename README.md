54c3574a9279fd5b927475986c982799af113f48
jordanliu <jordanxavierliu@gmail.com>
2020-02-13 23:11:51 -0500

    added antd

--- README.md
+++ README.md
@@ -8,20 +8,21 @@ CIT3009: Advanced Programming Group Project - built with MERN by Racquel Bailey 
 <br>
 
 ## Project Description
+
 1.  **Overview**
 
     **Title:** Enabling Services for the Disabled: A Cup of Uplifting Coffee (C.U.P) <br><br>
     Cups is a local coffee shop that provides a relaxing getaway in the middle of the city for the disabled community. They are also a wonderful example of a [Social Enterprise Boost Initiative ](https://www.micaf.gov.jm/msme-initiatives/`social-enterprise-boost-initiative-sebi)
-    similar to [DeafCan coffee](https://www.deafcancoffee.com/). Kat, the manager, has been encouraged by her mentor to establish another store at 
-    95 Moolean Avenue in the heart of Montego Bay. Kat would like to encourage an empowering environment through self service. Your consulting team providing pro bono services has 
-    considered to incorporate Artificial Intelligence through Computer Vision and Speech Processing to accomplish this. The touch-screen self service kiosk will allow customers to 
+    similar to [DeafCan coffee](https://www.deafcancoffee.com/). Kat, the manager, has been encouraged by her mentor to establish another store at
+    95 Moolean Avenue in the heart of Montego Bay. Kat would like to encourage an empowering environment through self service. Your consulting team providing pro bono services has
+    considered to incorporate Artificial Intelligence through Computer Vision and Speech Processing to accomplish this. The touch-screen self service kiosk will allow customers to
     order their favourite treats and verify using their Digital Id.
     <br><br>
     Link to [ERD](https://gitlab.com/jordanxliu/cups/blob/master/design/CUPS_ERD.pdf) <br>
     Link to [Wireframe](https://gitlab.com/jordanxliu/cups/blob/master/design/CUPS_WIREFRAME.pdf) <br>
     <br>
-    Group Lead: Jordan Liu 
-
+    Group Lead: Jordan Liu
+    Integration Lead: Jordan Liu
 
 ## Quick start
 
@@ -32,11 +33,13 @@ CIT3009: Advanced Programming Group Project - built with MERN by Racquel Bailey 
     ```shell
     cd cups/
     npm i
-    npm run dev
+    cd cups/client
+    npm i
+    npm run dev //runs client and server concurrently
+    npm run client //runs client only
+    npm run server //runs server only
     ```
 
 2.  **Open the source code and start editing!**
 
-    Your site is now running at `http://localhost:3000`! <br>
-
-
+    Your client is now running at `http://localhost:3000`! <br>
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Select, InputNumber, DatePicker, Switch, Slider, Button } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Option } = Select;
+
+const App = () => (
+  <>
+    <section style={{textAlign: 'center'}}>
+      <h1 style={{textAlign: 'center'}}>Ant Design</h1>
+      <img style={{width: '40px', height: '40px'}} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"/>
+    </section>
+    <Form style={{ marginTop: 32 }}>
+      <Form.Item
+        label="数字输入框"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <InputNumber min={1} max={10} defaultValue={3} />
+        <span className="ant-form-text"> 台机器</span>
+        <a href="https://ant.design">链接文字</a>
+      </Form.Item>
+      <Form.Item
+        label="开关"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <Switch defaultChecked />
+      </Form.Item>
+      <Form.Item
+        label="滑动输入条"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <Slider defaultValue={70} />
+      </Form.Item>
+      <Form.Item
+        label="选择器"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <Select defaultValue="lucy" style={{ width: 192 }}>
+          <Option value="jack">jack</Option>
+          <Option value="lucy">lucy</Option>
+          <Option value="disabled" disabled>disabled</Option>
+          <Option value="yiminghe">yiminghe</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="日期选择框"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item>
+        
+      </Form.Item>
+      <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          确定
+        </Button>
+        <Button style={{ marginLeft: 8 }}>
+          取消
+        </Button>
+      </Form.Item>
+    </Form>
+  </>
+);
 
 export default App;
