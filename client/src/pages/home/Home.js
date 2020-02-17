532e4226b1d72bcb05a1788be020780b973e5e2b
jordanliu <jordanxavierliu@gmail.com>
2020-02-16 21:56:41 -0500

    Added homepage UI

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'antd';
 
 const Home = () => {
     return (
         <div>
-            You are on home, click here to <Link to="/login">login</Link>
+            <Row>
+                <Col span={14} push={10} className="home-right"></Col>
+                <Col span={10} pull={14} className="home-left">
+                    <div>
+                        <h1>CUPS</h1>
+                        <h3>Select an option below to start ordering!</h3>
+                        <Link to="/login">
+                            <button>LOGIN</button>
+                        </Link>
+                        <Link to="/register">
+                            <button>REGISTER</button>
+                        </Link>
+                        <h3 className="home-footer">
+                            Need assistance? Ask an employee for help
+                        </h3>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
