c5873df336d041a5a5e34ae4645868a32f08c5fd
jordanliu <jordanxavierliu@gmail.com>
2020-02-16 22:15:23 -0500

    refactoring

--- client/src/pages/portalMenuOptions/menuAdd/MenuAdd.js
+++ client/src/pages/portalMenuOptions/menuAdd/MenuAdd.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Form, Select, Button, Input } from 'antd';
+import { Layout, Form, Select, Button, Input, Upload, Icon } from 'antd';
 import './MenuAdd.css';
 import PortalNav from '../../portalNav/PortalNav';
 
@@ -19,12 +19,15 @@ function onFocus() {
     console.log('focus');
 }
 
-const MenuAdd = () => {
+const MenuAdd = props => {
     return (
         <Layout style={{ minHeight: '100vh' }}>
             <PortalNav />
             <Layout>
-                <Content style={{ margin: '16px 16px', align: 'middle' }}>
+                <Content
+                    style={{ margin: '16px 16px', align: 'middle' }}
+                    className="menu-add-wrapper"
+                >
                     <h1>Add Item</h1>
                     <Form className="add-item-form">
                         <FormItem>
@@ -70,6 +73,29 @@ const MenuAdd = () => {
                                 placeholder="Item Cost"
                             />
                         </FormItem>
+                        <div className="menu-add-upload">
+                            <FormItem>
+                                <Upload {...props}>
+                                    <Button>
+                                        <Icon type="upload" /> Upload Photo
+                                    </Button>
+                                </Upload>
+                            </FormItem>
+                            <FormItem>
+                                <Upload {...props}>
+                                    <Button>
+                                        <Icon type="upload" /> Upload ASL
+                                    </Button>
+                                </Upload>
+                            </FormItem>
+                            <FormItem>
+                                <Upload {...props}>
+                                    <Button>
+                                        <Icon type="upload" /> Upload Audio
+                                    </Button>
+                                </Upload>
+                            </FormItem>
+                        </div>
                         <FormItem>
                             <Button
                                 type="primary"
--- client/src/pages/portalMenuOptions/menuUpdate/MenuUpdate.js
+++ client/src/pages/portalMenuOptions/menuUpdate/MenuUpdate.js
@@ -10,7 +10,10 @@ const MenuUpdate = () => {
         <Layout style={{ align: 'middle', minHeight: '100vh' }}>
             <PortalNav />
             <Layout>
-                <Content style={{ margin: '16px 16px' }}>
+                <Content
+                    style={{ margin: '16px 16px' }}
+                    className="menu-update-wrapper"
+                >
                     <h1>Update Item</h1>
 
                     <Form>
@@ -25,8 +28,16 @@ const MenuUpdate = () => {
                             <Input
                                 type="number"
                                 style={{ width: 200 }}
+                                id="itemCost"
+                                placeholder="Updated Cost"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                type="number"
+                                style={{ width: 200 }}
                                 id="stockQuantity"
-                                placeholder="Updated Stock Quantity"
+                                placeholder="Update Menu Item"
                             />
                         </Form.Item>
                         <Form.Item>
