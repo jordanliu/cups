1fb1585552d4668455c7eb423787f6cad44dfd7f
Jordan Liu <jordanxavierliu@gmail.com>
2020-03-06 00:04:52 -0500

    Added lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,67 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Portal from './pages/portal/Portal';
-import Login from './pages/login/Login';
-import Home from './pages/home/Home';
-import PortalMenu from './pages/portalMenu/PortalMenu';
-import MenuAdd from './pages/portalMenuOptions/menuAdd/MenuAdd';
-import MenuRemove from './pages/portalMenuOptions/menuRemove/MenuRemove';
-import MenuUpdate from './pages/portalMenuOptions/menuUpdate/MenuUpdate';
-import MenuView from './pages/portalMenuOptions/menuView/MenuView';
-import Menu from './pages/menu/Menu';
-import Order from './pages/order/Order';
-import OrderComplete from './pages/orderComplete/OrderComplete';
-
 import { GlobalProvider } from './context/GlobalState';
+import Loader from './pages/loader/Loader';
+
+const Portal = lazy(() => import('./pages/portal/Portal'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Home = lazy(() => import('./pages/home/Home'));
+const PortalMenu = lazy(() => import('./pages/portalMenu/PortalMenu'));
+const MenuAdd = lazy(() => import('./pages/portalMenuOptions/menuAdd/MenuAdd'));
+const MenuRemove = lazy(() =>
+    import('./pages/portalMenuOptions/menuRemove/MenuRemove')
+);
+const MenuUpdate = lazy(() =>
+    import('./pages/portalMenuOptions/menuUpdate/MenuUpdate')
+);
+const MenuView = lazy(() =>
+    import('./pages/portalMenuOptions/menuView/MenuView')
+);
+const Menu = lazy(() => import('./pages/menu/Menu'));
+const Order = lazy(() => import('./pages/order/Order'));
+const OrderComplete = lazy(() => import('./pages/orderComplete/OrderComplete'));
 
 const App = () => {
     return (
         <GlobalProvider>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Home}></Route>
-                    <Route path="/login" exact component={Login}></Route>
-                    <Route path="/menu" exact component={Menu}></Route>
-                    <Route path="/portal" exact component={Portal}></Route>
-                    <Route path="/order" exact component={Order}></Route>
-                    <Route
-                        path="/order-confirmed"
-                        component={OrderComplete}
-                    ></Route>
+            <Suspense fallback={<Loader />}>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/login" exact component={Login}></Route>
+                        <Route path="/menu" exact component={Menu}></Route>
+                        <Route path="/portal" exact component={Portal}></Route>
+                        <Route path="/order" exact component={Order}></Route>
+                        <Route
+                            path="/order-confirmed"
+                            component={OrderComplete}
+                        ></Route>
 
-                    <Route
-                        path="/portal-menu"
-                        exact
-                        component={PortalMenu}
-                    ></Route>
-                    <Route path="/portal-menu/add" component={MenuAdd}></Route>
-                    <Route
-                        path="/portal-menu/view"
-                        component={MenuView}
-                    ></Route>
-                    <Route
-                        path="/portal-menu/remove"
-                        component={MenuRemove}
-                    ></Route>
-                    <Route
-                        path="/portal-menu/update"
-                        component={MenuUpdate}
-                    ></Route>
-                </Switch>
-            </Router>
+                        <Route
+                            path="/portal-menu"
+                            exact
+                            component={PortalMenu}
+                        ></Route>
+                        <Route
+                            path="/portal-menu/add"
+                            component={MenuAdd}
+                        ></Route>
+                        <Route
+                            path="/portal-menu/view"
+                            component={MenuView}
+                        ></Route>
+                        <Route
+                            path="/portal-menu/remove"
+                            component={MenuRemove}
+                        ></Route>
+                        <Route
+                            path="/portal-menu/update"
+                            component={MenuUpdate}
+                        ></Route>
+                    </Switch>
+                </Router>
+            </Suspense>
         </GlobalProvider>
     );
 };
---
+++ client/src/pages/loader/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactComponent as LoaderAnim } from '../../images/coffee_animated.svg';
+import './Loader.css';
+const Loader = () => {
+    return <LoaderAnim className="loader-anim" />;
+};
+
+export default Loader;
